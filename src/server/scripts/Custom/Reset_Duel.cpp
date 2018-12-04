e0415cc5f6008dcca226daffe2a8c0e7c1395ee0
gragonvlad <clad1995@gmail.com>
2018-12-04 12:43:57 +0300

    ss
    ss

---
+++ src/server/scripts/Custom/Reset_Duel.cpp
@@ -0,0 +1,24 @@
+#include "ScriptPCH.h"
+ 
+class Shadow_Reset : public PlayerScript
+{
+        public:
+                Shadow_Reset() : PlayerScript("Shadow_Reset"){}
+ 
+                void OnDuelEnd(Player* pWinner, Player* pLoser, DuelCompleteType /*type*/)
+                {
+                        pWinner->RemoveAllSpellCooldown();
+                        pLoser->RemoveAllSpellCooldown();
+                        pWinner->SetHealth(pWinner->GetMaxHealth());
+                        if ( pWinner->getPowerType() == POWER_MANA )
+                                pWinner->SetPower(POWER_MANA, pWinner->GetMaxPower(POWER_MANA));
+                        pLoser->SetHealth(pLoser->GetMaxHealth());
+                        if ( pLoser->getPowerType() == POWER_MANA )
+                                pLoser->SetPower(POWER_MANA,  pLoser->GetMaxPower(POWER_MANA));
+                }
+};
+ 
+void AddSC_Shadow_Reset()
+{
+        new Shadow_Reset();
+}
